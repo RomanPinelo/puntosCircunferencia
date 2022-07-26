d4c30cc238a18c96aa63399236a3f0f635836e1a
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-07-26 15:07:36 -0500

    Ventana modal agregada

--- calculo.js
+++ calculo.js
@@ -9,6 +9,10 @@ var busca = document.getElementById("buscando");
 var btnPun = document.getElementById("btnPuntos");
 var btnBor = document.getElementById("btnBorrar");
 
+var ventanaModal = document.getElementById("modal");
+var btnModal = document.getElementById("cerrar");
+var textoModal = document.getElementById("mensajeModal");
+
 // Funciones
 function pintaEcuacion(h, k, radioCuadrado) {
   if (h < 0) {
@@ -28,45 +32,80 @@ function pintaEcuacion(h, k, radioCuadrado) {
   }
 }
 
+function verificaDatos(h, k, radioCuadrado) {
+  if (h == "") {
+    h = 0;
+  }
+  if (k == "") {
+    k = 0;
+  }
+  if (radioCuadrado == "") {
+    ventanaModal.classList.toggle("active");
+    textoModal.innerHTML = "Introduce un valor del radio al cuadrado.";
+  } else if (radioCuadrado == 0) {
+    ventanaModal.classList.toggle("active");
+    textoModal.innerHTML = "Introduce un valor del radio al cuadrado mayor a 0.";
+  } else if (radioCuadrado < 0) {
+    ventanaModal.classList.toggle("active");
+    textoModal.innerHTML = "El valor del radio al cuadrado no puede ser negativo. Introduce un número mayor a 0.";
+  } else {
+    let x = -100;
+    let y = -100;
+    let resultado;
+
+    pintaEcuacion(h, k, radioCuadrado);
+
+    busca.innerHTML = "Buscando puntos...";
+
+    var id = setInterval(() => {
+      resultado = (Math.pow((x - (h)), 2)) + (Math.pow((y - (k)), 2));
+
+      if (resultado == radioCuadrado) {
+        tabla.innerHTML += `
+        <tr>
+          <td>${x}</td>
+          <td>${y}</td>
+        </tr>
+        `;
+      }
+
+      y++;
+
+      if (y > 100) {
+        y = -100;
+        x++;
+        if (x > 100) {
+          busca.innerHTML = "";
+          mensajeNoResultados.innerHTML = "No hay más resultados";
+          clearInterval(id);
+        }
+      }
+    }, 1);
+  }
+}
+
 // Evento de los botones
 btnBor.addEventListener('click', function(){
   window.location.reload();
 });
 
+btnModal.addEventListener('click', function(){
+  ventanaModal.classList.toggle("active");
+});
+
 btnPun.addEventListener('click', function(){
-  let x = -100;
-  let y = -100;
   let h = coorH.value;
   let k = coorK.value;
   let radioCuadrado = valorRadioCuadrado.value;
-  let resultado;
 
-  pintaEcuacion(h, k, radioCuadrado);
+  ecuaCanonica.innerHTML = "";
+  mensajeNoResultados.innerHTML = "";
+  tabla.innerHTML = `
+    <tr>
+      <th>x</th>
+      <th>y</th>
+    </tr>
+  `;
 
-  busca.innerHTML = "Buscando puntos...";
-
-  var id = setInterval(() => {
-    resultado = (Math.pow((x - (h)), 2)) + (Math.pow((y - (k)), 2));
-
-    if (resultado == radioCuadrado) {
-      tabla.innerHTML += `
-      <tr>
-        <td>${x}</td>
-        <td>${y}</td>
-      </tr>
-      `;
-    }
-
-    y++;
-
-    if (y > 100) {
-      y = -100;
-      x++;
-      if (x > 100) {
-        busca.innerHTML = "";
-        mensajeNoResultados.innerHTML = "No hay más resultados";
-        clearInterval(id);
-      }
-    }
-  }, 1);
+  verificaDatos(h, k, radioCuadrado);
 });
